6c24645983eac116e4ed804202603460a5d6dfd3
arnaudcrestey <arnaud.crestey14@gmail.com>
2026-07-09 13:52:54 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -73,7 +73,7 @@ export default function Page() {
 
           <p className="hero-positioning-text">
             J&apos;aide les dirigeants à comprendre ce qui freine réellement
-            leur développement avant de concevoir les solutions les plus adaptés.
+            leur développement avant de concevoir les solutions les plus adaptées.
           </p>
         </section>
 
